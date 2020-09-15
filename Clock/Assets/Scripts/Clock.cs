aaa11ca29a4099b4b965bfbb0d7fd6d4d86c5102
Mohan Krishna <MohanKrishnaKanth@users.noreply.github.com>
2020-09-15 17:24:02 +0530

    clock project

---
+++ Clock/Assets/Scripts/Clock.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clock : MonoBehaviour
+{
+    public Transform hoursArm, minutesArm, secondsArm;
+    const float degreesPerHour = 30f;
+    const float degreesPerMinuteAndSeconds = 6f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        TimeSpan time = DateTime.Now.TimeOfDay;
+        hoursArm.transform.localRotation = Quaternion.Euler(0f, (float)time.TotalHours * degreesPerHour, 0f);
+        minutesArm.transform.localRotation = Quaternion.Euler(0f, (float)time.TotalMinutes * degreesPerMinuteAndSeconds, 0f);
+        secondsArm.transform.localRotation = Quaternion.Euler(0f, (float)time.TotalSeconds * degreesPerMinuteAndSeconds, 0f);
+        
+    }
+}
